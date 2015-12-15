b413b910362756e4fd3216e1439148eb0025f81a
lightweave <brilkov@gmail.com>
2015-12-15 11:19:25 +0300

    Revert "Revert "export from doc""
    This reverts commit cd37ca2c9e8293bd726d1091235feb373082fb25.

---
+++ _data/14FF901780C2R5QBIU4JO77BGQX6SGTPBOF8/My Drafts/mu.mm
@@ -0,0 +1,68 @@
+<map version="docear 1.1" dcr_id="1443179739352_efp14k3jln8s2cklcqb3m75pd" project="14FF901780C2R5QBIU4JO77BGQX6SGTPBOF8" project_last_home="file:/D:/Ivan/_flash%20backup%202014/TEX/thesis/">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="Cosmic ray mu- in polar reigns" FOLDED="false" ID="ID_1505972454" CREATED="1443179739226" MODIFIED="1443179804512">
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="geant simple project" POSITION="right" ID="ID_1405917301" CREATED="1443179825928" MODIFIED="1443179887048">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="geant atmocosmics" POSITION="right" ID="ID_578118595" CREATED="1443179888517" MODIFIED="1443179918200">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="literature" POSITION="right" ID="ID_1659755012" CREATED="1443179899122" MODIFIED="1443179934740">
+<edge COLOR="#00ff00"/>
+<node TEXT="classics" ID="ID_1175598544" CREATED="1443179978274" MODIFIED="1443179986424"/>
+<node TEXT="&#x421;&#x442;&#x43e;&#x436;&#x43a;&#x43e;&#x432; &#x42e;&#x440;&#x438;&#x439; &#x438;&#x432;&#x430;&#x43d;&#x43e;&#x432;&#x438;&#x447;" ID="ID_867320291" CREATED="1443179988746" MODIFIED="1443180040487"/>
+<node TEXT="" ID="ID_1124434896" CREATED="1443180042112" MODIFIED="1443180042112"/>
+</node>
+</node>
+</map>
